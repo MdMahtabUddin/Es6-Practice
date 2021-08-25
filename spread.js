005eb3e3a17afb7198391dc131716d8a91763e42
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-08-25 22:48:28 +0600

    first

--- spread.js
+++ spread.js
@@ -1,6 +1,6 @@
 const numbers = [23, 65, 99, 21, 34];
 // console.log(numbers);
-// console.log(...numbers);
+console.log(...numbers);
 
 const max = Math.max(23, 99, 65, 21, 34);
 const maxInArray = Math.max(...numbers);
